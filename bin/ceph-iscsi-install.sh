26ba29db3eb33423e65d5d7a0ac1f4fb7d5d8e03
Ricardo Marques <rimarques@suse.com>
2018-11-21 12:28:07 +0000

    Rename ceph-iscsi-* scripts
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- bin/ceph-iscsi-install.sh
+++ bin/ceph-iscsi-install.sh
@@ -5,12 +5,6 @@ sudo pkill -f 'sudo /usr/bin/rbd-target-gw'
 sudo systemctl stop rbd-target-api
 sudo pkill -f 'sudo /usr/bin/rbd-target-api'
 
-sudo targetcli clearconfig confirm=True
-ceph osd pool delete rbd rbd --yes-i-really-really-mean-it
-sleep 3
-if ! rados lspools | grep -q '^rbd$'; then
-  ceph osd pool create rbd 1 1
-fi
 sudo python setup.py install
 
 sudo /usr/bin/rbd-target-gw &
--- bin/ceph-iscsi-reset.sh
+++ bin/ceph-iscsi-reset.sh
@@ -1,10 +1,16 @@
 cd ~/ceph-iscsi
 
-#sudo systemctl stop rbd-target-gw
+sudo systemctl stop rbd-target-gw
 sudo pkill -f 'sudo /usr/bin/rbd-target-gw'
-#sudo systemctl stop rbd-target-api
+sudo systemctl stop rbd-target-api
 sudo pkill -f 'sudo /usr/bin/rbd-target-api'
 
+sudo targetcli clearconfig confirm=True
+ceph osd pool delete rbd rbd --yes-i-really-really-mean-it
+sleep 3
+if ! rados lspools | grep -q '^rbd$'; then
+  ceph osd pool create rbd 1 1
+fi
 sudo python setup.py install
 
 sudo /usr/bin/rbd-target-gw &
