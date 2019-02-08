3f7699212b08ee185d47b7916f7d7bd4de85f81a
Ricardo Marques <rimarques@suse.com>
2019-02-08 13:33:50 +0000

    Improve ceph-iscsi-reset.sh
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- bin/ceph-iscsi-reset.sh
+++ bin/ceph-iscsi-reset.sh
@@ -1,12 +1,8 @@
 cd ~/ceph-iscsi
 
 sudo targetcli clearconfig confirm=True
-ceph osd pool delete rbd rbd --yes-i-really-really-mean-it
-sleep 3
-if ! rados lspools | grep -q '^rbd$'; then
-  ceph osd pool create rbd 1 1
-  rbd pool init rbd
-fi
+rados -p rbd rm gateway.conf
+
 sudo python3 setup.py install
 
 sudo systemctl reset-failed
