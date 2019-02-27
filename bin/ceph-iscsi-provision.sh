90c54d2125eed9bc0906dc020eae8351a88731c0
Ricardo Dias <rdias@suse.com>
2019-02-27 19:04:18 +0000

    Create custom keyring for each iscsi node
    Signed-off-by: Ricardo Dias <rdias@suse.com>

--- bin/ceph-iscsi-provision.sh
+++ bin/ceph-iscsi-provision.sh
@@ -14,16 +14,27 @@ if [ -z $1 ]; then
   sudo python3 setup.py install
   sudo cp /home/vagrant/ceph-iscsi/usr/lib/systemd/system/rbd-target-gw.service /usr/lib/systemd/system
   sudo cp /home/vagrant/ceph-iscsi/usr/lib/systemd/system/rbd-target-api.service /usr/lib/systemd/system
+  sudo mkdir -p /usr/lib/systemd/system/rbd-target-gw.service.d
+  sudo cp /home/vagrant/ceph-iscsi/etc/systemd/system/rbd-target-gw.service.d/* /usr/lib/systemd/system/rbd-target-gw.service.d/
   sudo systemctl daemon-reload
 else
   sudo zypper -n install ceph-iscsi
 fi
 
+HOST=`hostname -s`
+
+ceph auth get-or-create client.igw.${HOST} \
+                        mon 'allow *' \
+                        osd 'allow *' \
+                        mgr 'allow r' \
+                        -o /etc/ceph/ceph.client.igw.${HOST}.keyring
+
 cat > /etc/ceph/iscsi-gateway.cfg <<EOF
 # http://docs.ceph.com/docs/master/rbd/iscsi-target-cli/
 [config]
 cluster_name = ceph
-gateway_keyring = ceph.client.admin.keyring
+gateway_keyring = ceph.client.igw.${HOST}.keyring
+cluster_client_name = client.igw.${HOST}
 api_secure = false
 api_user = admin
 api_password = admin
@@ -45,10 +56,7 @@ ceph dashboard iscsi-gateway-list
 # tcmu-runner
 sudo systemctl enable tcmu-runner
 sudo systemctl restart tcmu-runner
-# ceph-iscsi-gw
+# ceph-iscsi
 sudo systemctl enable rbd-target-gw
-sudo systemctl restart rbd-target-gw
-# ceph-iscsi-api
 sudo systemctl enable rbd-target-api
-sudo systemctl restart rbd-target-api
-
+sudo systemctl restart rbd-target-gw  # this will also start rbd-target-api
