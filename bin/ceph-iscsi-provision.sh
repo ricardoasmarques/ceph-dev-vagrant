e6b1b11d1ecea41014479217c36072f41b82a1d7
Ricardo Marques <rimarques@suse.com>
2019-03-06 12:48:25 +0000

    Adds configuration for password encryption
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- bin/ceph-iscsi-provision.sh
+++ bin/ceph-iscsi-provision.sh
@@ -29,7 +29,12 @@ api_user = admin
 api_password = admin
 api_port = 5001
 trusted_ip_list = 192.168.100.201,192.168.100.202,192.168.100.203,192.168.100.1
+# Uncomment this to enable password encryption
+# priv_key = keys/private_key
+# pub_key = keys/public_key
 EOF
+
+sudo cp -r /home/vagrant/keys /etc/ceph
 
 if ! rados lspools | grep -q '^rbd$'; then
   ceph osd pool create rbd 50
