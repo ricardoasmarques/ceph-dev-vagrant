ae2ca13edaaee0c21b18688d4f34ff00f63802bb
Ricardo Marques <rimarques@suse.com>
2019-03-13 15:49:42 +0000

    Fix create target script
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- bin/iscsi-create-target.sh
+++ bin/iscsi-create-target.sh
@@ -5,7 +5,7 @@ gwcli /iscsi-targets/iqn.2003-01.com.redhat.iscsi-gw:iscsi-igw/gateways create n
 gwcli /iscsi-targets/iqn.2003-01.com.redhat.iscsi-gw:iscsi-igw/gateways create node2 192.168.100.202 skipchecks=true
 gwcli /disks create pool=rbd image=disk_1 size=90G
 gwcli /iscsi-targets/iqn.2003-01.com.redhat.iscsi-gw:iscsi-igw/hosts create iqn.1994-05.com.redhat:rh7-client
-gwcli /iscsi-targets/iqn.2003-01.com.redhat.iscsi-gw:iscsi-igw/hosts/iqn.1994-05.com.redhat:rh7-client disk add rbd.disk_1
+gwcli /iscsi-targets/iqn.2003-01.com.redhat.iscsi-gw:iscsi-igw/hosts/iqn.1994-05.com.redhat:rh7-client disk add rbd/disk_1
 gwcli /iscsi-targets/iqn.2003-01.com.redhat.iscsi-gw:iscsi-igw/hosts auth nochap
 
 gwcli ls
